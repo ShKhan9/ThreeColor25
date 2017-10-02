1294103da43021b7bab4ed9c23fad4d78bcedabc
shehata <shehatagamal120@yahoo.com>
2017-10-02 13:52:28 +0200

    Initial Commit

---
+++ ThreeColor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ThreeColor
+//
+//  Created by ShKhan on 10/2/17.
+//  Copyright © 2017 Approcks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
